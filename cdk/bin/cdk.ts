09f8c661cdf33d8ba19856e6c9220c8bdf7e65da
Robert Foster <foster318@gmail.com>
2025-08-12 18:17:45 -0400

    Delete cdk directory

--- cdk/bin/cdk.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
-import { CoreStack } from '../lib/core-stack.js';
-
-const app = new App();
-new CoreStack(app, 'ShowChoirBillingStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION ?? 'us-east-2' },
-});
