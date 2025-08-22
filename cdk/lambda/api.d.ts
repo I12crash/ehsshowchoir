4f62e59425b2ae1376bf794074b4bc4e0191deb1
Robert Foster <foster318@gmail.com>
2025-08-21 21:17:13 -0400

    Delete cdk directory

--- cdk/lambda/api.d.ts
+++
@@ -1,2 +0,0 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Context } from 'aws-lambda';
-export declare const handler: (event: APIGatewayProxyEventV2, context: Context) => Promise<APIGatewayProxyResultV2>;
--- cdk/lib/foster-dev-stack.d.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-export interface FosterDevStackProps extends cdk.StackProps {
-    hostedUiPrefix: string;
-}
-export declare class FosterDevStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: FosterDevStackProps);
-}
